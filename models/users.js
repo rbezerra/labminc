e67aa15a78a3112075ce993800196c8e7543e2d8
Ramon Bezerra <ramonbezerra90@gmail.com>
2016-04-18 21:52:39 -0300

    first blood

---
+++ models/users.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataType) => {
+	const Users = sequelize.define("Users", {
+		id: {
+			type: DataType.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		name: {
+			type: DataType.STRING,
+			allowNull: false,
+			validate: {
+				notEmpty: true
+			}
+		},
+		email: {
+			type: DataType.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				notEmpty: true
+			}
+		}
+	}, {
+		classMethods: {
+			associate: (models) => {
+				Users.hasMany(models.Tasks);
+			}
+		}
+	});
+	return Users;
+}
