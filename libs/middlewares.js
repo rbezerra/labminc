eb985b3a24b3fd8e3dcf3291b545337574d99e2c
Ramon Bezerra <ramonbezerra90@gmail.com>
2016-05-09 00:10:00 -0300

    finalizando documentação da api

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,4 +1,5 @@
-const bodyParser = require("body-parser");
+import bodyParser from "body-parser";
+import express from "express";
 module.exports = app => {
 	app.set("port", 3000);
 	app.set("json spaces", 4);
@@ -7,5 +8,6 @@ module.exports = app => {
 	app.use((req, res, next) => {
 		delete req.body.id;
 		next();
-	})
+	});
+	app.use(express.static("public"));
 };
---
+++ public/apidoc/api_project.js
@@ -0,0 +1,13 @@
+define({
+  "name": "Documentação - Node Task API",
+  "version": "1.0.0",
+  "description": "API de Gestão de Tarefas",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-05-09T03:08:18.332Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
